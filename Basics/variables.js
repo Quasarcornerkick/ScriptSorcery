93245ceace5f53bfaef1f7e46e987432e5ebbc38
Quasarcornerkick <kashyap.vivek23.2@gmail.com>
2024-08-06 06:21:38 +0000

    Variables part1

---
+++ Basics/variables.js
@@ -0,0 +1,19 @@
+const accountId = 1223581
+let accountEmail = "morantblaine@gmail.com"
+var accountPassword = 4452423562
+accountCity = "Los Angeles"
+let accountState 
+
+// accountId =24324252
+console.log(accountId);
+accountEmail = "tsa@gmail.com"
+accountPassword = 44353246242
+accountCity ="Boston"
+/* Prefferable not to use var for declaring variables but instead use LET!
+var shouldn't be used as it does not recognize scope
+
+And a variable defined using let, when hasn't been initialized will be shown to contain the value "undefined"
+
+*/
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
