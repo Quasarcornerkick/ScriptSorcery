500ea7eda16a32d28bb119862eb46640c3df891b
Quasarcornerkick <kashyap.vivek23.2@gmail.com>
2024-08-06 07:15:31 +0000

    types of data

---
+++ Basics/datatypes.js
@@ -0,0 +1,20 @@
+"use script"; // treat the code as the newest version
+// alert(4+5) using it in nodejs not in browser
+console.log("Armie")
+
+let name = "Armie"
+let age =129
+let isloggedIn = false
+
+/*
+number =>2 raised to power 52
+string=> ""
+boolean => true or false
+null => standalone value
+undefined => undefined type of value
+symbol => for unique values
+object =>
+*/
+
+console.log(typeof undefined)// o/p would be undefined type
+console.log(typeof null)// o/p would be null object
